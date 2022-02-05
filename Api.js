880f6f7494eea99dfc45cc159cb6883c97e7cf89
Naqash-Muhammad <92191163+Naqash-Muhammad@users.noreply.github.com>
2022-02-05 02:21:07 -0700

    SaniAA

---
+++ Api.js
@@ -0,0 +1,49 @@
+[
+    {
+        "id" : 0,
+       " Name" : "Naqash",
+        "Age" : 22,
+        "Edu" : "BsCs",
+        "Adress" : {
+            "City" : "Lahore",
+            "Street" : "Rehman Pura",
+            "Houes" : "12 Num"
+        }
+    },
+
+    {
+        "id" : 1,
+       " Name" : "Naqash",
+        "Age" : 22,
+        "Edu" : "BsCs",
+        "Adress" : {
+            "City" : "Lahore",
+            "Street" : "Rehman Pura",
+            "Houes" : "12 Num"
+        }
+    },
+
+    {
+        "id" : 2,
+       " Name" : "Naqash",
+        "Age" : 22,
+        "Edu" : "BsCs",
+        "Adress" : {
+            "City" : "Lahore",
+            "Street" : "Rehman Pura",
+            "Houes" : "12 Num"
+        }
+    },
+
+    {
+        "id" : 3,
+       " Name" : "Naqash",
+        "Age" : 22,
+        "Edu" : "BsCs",
+        "Adress" : {
+            "City" : "Lahore",
+            "Street" : "Rehman Pura",
+            "Houes" : "12 Num"
+        }
+    },
+]
